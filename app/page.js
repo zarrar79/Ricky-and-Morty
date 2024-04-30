8a2cfb333609b6c96cc57526c9832a9c073b018c
zarrar79 <shahzarrar79@gmail.com>
2024-04-30 17:24:32 +0500

    loader added

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 'use client'
-import { all } from 'axios';
-import { Allerta_Stencil, Inclusive_Sans } from 'next/font/google';
 import Link from 'next/link';
-import PreviousMap from 'postcss/lib/previous-map';
 import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import { ColorRing } from 'react-loader-spinner'
 
 export default function Page() {
   const [data, setData] = useState([]);
-  const [currPage,setCurrPage] = useState(1);
+  const [currPage, setCurrPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(0);
   const [searchPram] = useState(["name", 'gender']);
   const [display, setDisplay] = useState('');
@@ -18,7 +16,7 @@ export default function Page() {
   const itemsPerPage = 9;
 
   useEffect(() => {
-    let fetchStartTime = Date.now(); // Record the start time of data fetching
+    let fetchStartTime = Date.now();
 
     // Function to fetch data
     const fetchData = async () => {
@@ -37,12 +35,12 @@ export default function Page() {
         }
 
         setData(allData);
-        console.log('Fetched data:', allData); // Log the fetched data
+        console.log('Fetched data:', allData);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
-        let fetchEndTime = Date.now(); // Record the end time of data fetching
-        let fetchDuration = fetchEndTime - fetchStartTime; // Calculate duration of data fetching
+        let fetchEndTime = Date.now();
+        let fetchDuration = fetchEndTime - fetchStartTime;
 
         // Show loading message if fetching duration is less than 1000 milliseconds (1 second), otherwise hide it
         console.log('Fetch duration:', fetchDuration);
@@ -67,11 +65,12 @@ export default function Page() {
 
   const search = (e) => {
     return data.filter((item) => {
-      if (((item.name).toLowerCase().indexOf((e.value).toLowerCase()))>-1){
+      if (((item.name).toLowerCase().indexOf((e.value).toLowerCase())) > -1) {
         return searchPram.some((newItem) => {
           return item[newItem].toString().toLowerCase().includes(e.value.toLowerCase());
         });
-      }});
+      }
+    });
   };
   const searchBtn = (e) => {
     e.preventDefault();
@@ -85,7 +84,7 @@ export default function Page() {
       }
     });
   };
-  
+
   const handleBtn = (e) => {
     setFilter(e.target.value);
     if (e.target.value == "all") {
@@ -97,81 +96,98 @@ export default function Page() {
       setDisplay(filteredData); // Assuming `setDisplay` is a state setter function for `displayData`
     }
   };
-  
-  const handleSearch = (e)=>{
-    console.log(e.value , "------>e")
-    if(e.value==="all"){
+
+  const handleSearch = (e) => {
+    console.log(e.value, "------>e")
+    if (e.value === "all") {
       setCurrPage(1);
       setCurrentPage(0);
-    displayData = data;
+      displayData = data;
     }
-  else{
-   displayData =  search(e);
-  setDisplay(displayData);
-   }
+    else {
+      displayData = search(e);
+      setDisplay(displayData);
+    }
   }
+
   return (
     <>
-    {loading? "Loading Data......":
-      <div className="pt-[0.1px] bg-[black]">
-        <div className="text-[white] flex justify-center text-[50px] font-[400] mt-3"><h1>Ricky and Morty</h1></div>
-        <div className="max-w-[1100px] mx-auto">
-          <div className="flex justify-center w-[100%] items-center bg-[black] mt-10">
-            <div className="rounded border-[2px] border-[solid] border-[#79c187] w-[30%]">
-              <input onChange={(e)=>{handleSearch(e.target)}} className="w-[100%] py-2 px-2" type='text' placeholder="Search by name" />
-            </div>
-            <div><select onChange={(e) => handleBtn(e)} className="text-white bg-[#6060e0] p-[9.5px] ml-4 rounded">
-      <option value="all">Filter by gender</option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-      <option value="unknown">Unknown</option>
-    </select></div>
-          </div>
-          <div className="pt-[0.1px] bg-[black]">
-            <div className="flex flex-wrap items-center justify-between bg-[#333232] px-2 mt-10">
-              {displayData?.map((card) => (
-                <div className="mt-4" key={card.id}>
-                    <Link href={`pages/character/${card.id}`}>
-                  <div><img className="rounded-l-[5px] rounded-r-[5px]" src={card.image} alt={card.name} /></div>
-                  <div className="bg-[#3c3e44] items-center text-center pt-4 pb-4">
-                    <div>
-                      <div className="text-[25px] font-bold text-[white]">{card.name}</div>
-                      <div className="text-[white]">{card.status + ' - ' + card.gender}</div>
-                    </div>
-                    <div>
-                      <div className="text-[#d1cdcd]">Last Known location:</div>
-                      <div className="text-[white]"><a href={card.location.url}>{card.location.name}</a></div>
-                    </div>
-                    <div>
-                      <div className="text-[#d1cdcd]">First seen in:</div>
-                      <div className="text-[white]"><a href={card.origin.url}>{card.origin.name}</a></div>
-                    </div>
-                    <div>
-                      <div className="text-[#d1cdcd]">Species</div>
-                      <div className="text-[white]">{card.species}</div>
-                    </div>
-                  </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen bg-black">
+          <ColorRing
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="color-ring-loading"
+  wrapperStyle={{}}
+  wrapperClass="color-ring-wrapper"
+  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+  />
         </div>
-        {data && (
-          <div>
-            <ReactPaginate className='flex justify-between max-w-[500px] mx-auto text-white mt-9 pb-9'
-              previousLabel={'Previous'}
-              nextLabel={'Next'}
-              breakLabel={'...'}
-              pageCount={!display ? Math.ceil(data.length / itemsPerPage):Math.ceil(display.length / itemsPerPage)}
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={5}
-              onPageChange={handlePageClick}
-            />
+      ) : (
+        <div className="pt-[0.1px] bg-[black]">
+          <div className="text-[white] flex justify-center text-[50px] font-[400] mt-3"><h1>Ricky and Morty</h1></div>
+          <div className="max-w-[1100px] mx-auto">
+            <div className="flex justify-center w-[100%] items-center bg-[black] mt-10">
+              <div className="rounded border-[2px] border-[solid] border-[#79c187] w-[30%]">
+                <input onChange={(e) => { handleSearch(e.target) }} className="w-[100%] py-2 px-2" type='text' placeholder="Search by name" />
+              </div>
+              <div><select onChange={(e) => handleBtn(e)} className="text-white bg-[#6060e0] p-[9.5px] ml-4 rounded">
+                <option value="all">Filter by gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="unknown">Unknown</option>
+              </select></div>
+            </div>
+            <div className="pt-[0.1px] bg-[black] min-h-[58vh]">
+              <div className="flex flex-wrap justify-around bg-[#333232] px-2 mt-10">
+                {displayData.length > 0 ? (
+                  displayData.map((card) => (
+                    <div className="mt-4" key={card.id}>
+                      <Link href={`/pages/character/${card.id}`}>
+                        <div><img className="rounded-l-[5px] rounded-r-[5px]" src={card.image} alt={card.name} /></div>
+                        <div className="bg-[#3c3e44] items-center text-center pt-4 pb-4">
+                          <div>
+                            <div className="text-[25px] font-bold text-[white]">{card.name}</div>
+                            <div className="text-[white]">{card.status + ' - ' + card.gender}</div>
+                          </div>
+                          <div>
+                            <div className="text-[#d1cdcd]">Last Known location:</div>
+                            <div className="text-[white]"><a href={card.location.url}>{card.location.name}</a></div>
+                          </div>
+                          <div>
+                            <div className="text-[#d1cdcd]">First seen in:</div>
+                            <div className="text-[white]"><a href={card.origin.url}>{card.origin.name}</a></div>
+                          </div>
+                          <div>
+                            <div className="text-[#d1cdcd]">Species</div>
+                            <div className="text-[white]">{card.species}</div>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  ))
+                ) : (
+                  <div className="text-white">No characters found</div>
+                )}
+              </div>
+            </div>
           </div>
-        )}
-      </div>
-      }
+          {data && (
+            <div>
+              <ReactPaginate className='flex justify-between max-w-[500px] mx-auto text-white mt-9 pb-9'
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                pageCount={!display ? Math.ceil(data.length / itemsPerPage) : Math.ceil(display.length / itemsPerPage)}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+              />
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
--- app/pages/character/[id]/page.js
+++ app/pages/character/[id]/page.js
@@ -1,22 +1,67 @@
+// pages/character/[id].js
+'use client'
+import React from 'react';
+import { useParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-export default function character({ post }) {
+export default function CharacterPage() {
+  const param = useParams();
+  const [episodes,setEpisodes] = useState(0);
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://rickandmortyapi.com/api/character/${param.id}`);
+        if (!res.ok) {
+          throw new Error('Failed to fetch character data');
+        }
+        const data = await res.json();
+        setCharacter(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  });
+
+  if (!character) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>{post}</>
+    <div className='bg-black min-h-[100vh] mt-[0.1px]'>
+      {/* character */}
+      <div className='max-w-[20%] mx-auto flex justify-center pt-[4rem]'>
+        <div key={character.id}>
+                  <div className='mx-auto'><img className="flex justify-center rounded-l-[5px] rounded-r-[5px]" src={character.image} alt={character.name} /></div>
+                  <div className="bg-[#3c3e44] items-center text-center pt-4 pb-4">
+                    <div>
+                      <div className="text-[25px] font-bold text-[white]">{character.name}</div>
+                      <div className="text-[white]">{character.status + ' - ' + character.gender}</div>
+                    </div>
+                    <div>
+                      <div className="text-[#d1cdcd]">Last Known location:</div>
+                      <div className="text-[white]"><a href={character.location.url}>{character.location.name}</a></div>
+                    </div>
+                    <div>
+                      <div className="text-[#d1cdcd]">First seen in:</div>
+                      <div className="text-[white]"><a href={character.origin.url}>{character.origin.name}</a></div>
+                    </div>
+                    <div>
+                      <div className="text-[#d1cdcd]">Species</div>
+                      <div className="text-[white]">{character.species}</div>
+                    </div>
+                  </div>
+                </div>
+      </div>
+      {/* back button */}
+      <Link href={`/`}>
+        <div className='flex justify-center'>
+        <button className='text-white px-[86.8px] py-3 bg-[#378e37]'>Go to main</button>
+        </div>
+      </Link>
+    </div>
   );
-}
-
-export async function getStaticPaths() {
-  const res = await fetch(`https://rickandmortyapi.com/api/character`);
-  const posts = await res.json();
-
-  const paths = posts.map((post) => ({
-    params: { id: post.id.toString() },
-  }));
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-  const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
-  const post = await res.json();
-  return { props: { post } };
 }
